d8b8a0d22feb2c19c3c510de0163ac939dc4688c
Anurag Verma <anuragv.1020@gmail.com>
2023-08-13 19:07:42 +0530

    adding multiple parsable gohtmls to template

--- web/templates/parse-html/parse-multiple/main.go
+++ web/templates/parse-html/parse-multiple/main.go
@@ -17,8 +17,9 @@ func main() {
 	}
 	tmpl, _ := template.ParseFiles("test2.gohtml") // returns Template pointer
 	// we can add more templates if we want later
+	tmpl, _ = tmpl.ParseFiles("test3.gohtml")
+
 	_ = tmpl.ExecuteTemplate(os.Stdout, "test2.gohtml", p)
 
-	tmpl, _ = template.ParseFiles("test3.gohtml")
 	_ = tmpl.ExecuteTemplate(os.Stdout, "test3.gohtml", p)
 }
